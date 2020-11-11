ba493d549f327ba9035baeb88114b29a5eee1d4d
vuongvgc <vuongvgc@gmail.com>
2020-11-11 21:22:57 +0700

    build reducer: songList & songSelected

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { selectSong } from '../actions/index';
 const App = () => {
+    //Return Action
     return(
         <div>
             App
---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+//Reducer Song List
+const songListReducer = () => {
+    return [
+        {song: "Faded", duration: "4.02"},
+        {song: "Alone", duration: "3.02"},
+        {song: "Sugar", duration: "5.02"},
+        {song: "Goi Xoi Voi", duration: "1.02"}
+    ]
+}
+// Reducer song Selected
+const songSelectedReducer = (songSelected = null, action) => {
+    if(action.type === "SONG_SELECTED"){
+        return  action.payload;
+    }
+    return songListReducer;
+}
