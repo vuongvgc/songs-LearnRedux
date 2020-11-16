941db42500d538339cff556db7041c544cbd4495
vuongvgc <vuongvgc@gmail.com>
2020-11-16 23:09:24 +0700

    condition rendering

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongDetail = (props) => {
-    console.log(props);
+const SongDetail = ( { mySelectedSong } ) => {
+    // console.log(props.mySelectedSong.title);
+    if(!mySelectedSong){
+        return (
+            <div>Select Song</div>
+        )
+    }
     return (
-        <div>SongDetail</div>
+        <div>
+            <h3>Song Detail:</h3>
+            <p>Title: {mySelectedSong.title}</p>
+            <p>Duration: {mySelectedSong.duration}</p>
+        </div>
     )
 }
 const mapStateToProps = (state) => {
-    return {song: state.songSelected}
+    return {mySelectedSong: state.songSelected}
 }
 export default connect(mapStateToProps)(SongDetail);
