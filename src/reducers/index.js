c17f1a9fad489beb64a071671af41af43c12d1fa
vuongvgc <vuongvgc@gmail.com>
2020-11-17 13:08:18 +0700

    review project

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,11 @@
+// TODO 2: tạo 1 reducer function and combine lại
 import { combineReducers } from 'redux';
-//Reducer Song List
+//  Create Reducer function (currentState, action object) => sẽ quyết định là có update hay không
+//Reducer function Song List 
+/**
+ *Function: songListReducer
+ *Return: state mới 
+ */ 
 const songListReducer = () => {
     return [
         {title: "Faded", duration: "4.02"},
@@ -8,13 +14,21 @@ const songListReducer = () => {
         {title: "Goi Xoi Voi", duration: "1.02"}
     ]
 }
-// Reducer song Selected
+// Reducer function song Selected
+/**
+ *Function: songSelectedReducer
+ *Input: current state : songSelected , action object: 
+ *Return: state mới or không có
+ */ 
 const songSelectedReducer = (songSelected = null, action) => {
     if(action.type === "SONG_SELECTED"){
+        // return 1 object
         return  action.payload;
     }
+    // không thay đổi
     return songSelected;
 }
+// combine nhiều reducer 
 export default combineReducers({
     songs: songListReducer,
     songSelected: songSelectedReducer
